0c1b9fe45e60d969ec69a8ae8ecd7a520e9cd5c9
unknown <clementherpe@gmail.com>
2022-03-06 16:05:05 +0100

    Verification des données envoyés et erreurs

--- models/books.js
+++ models/books.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
 const Author = require('./author');
+const yup = require('yup');
+
 //BOOK SCHEMA
 const BookSchema = new mongoose.Schema({
     name: {
@@ -17,4 +19,20 @@ const BookSchema = new mongoose.Schema({
     }
 });
 
-module.exports = new mongoose.model('Book', BookSchema);
+const validateBook = book => {
+    const schema = yup.object().shape({
+        bookName: yup.string().required().min(3).max(50),
+        authorName: yup.string().required().min(3).max(50),
+        //Pour message perso selon l'erreur
+        authorAge: yup.number().required().min(10, "Ecrire un livre avant 10 ans ? Impossible !").max(100),
+        gender:  yup.string().required().min(3).max(50),
+    });
+    return schema.validate(book).then(book => book).catch(error => {
+        return {
+            message: error.message
+        }
+    });
+}
+
+exports.Book = new mongoose.model('Book', BookSchema);
+exports.validateBook = validateBook;
