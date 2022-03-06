9eb530b575f0f695305404eb1016079405bbccb5
unknown <clementherpe@gmail.com>
2022-03-06 16:29:15 +0100

    REST actions en place

--- routes/books.js
+++ routes/books.js
@@ -22,4 +22,40 @@ router.post('/', async (req,res) => {
     })
 });
 
+//GET ALL BOOKS
+router.get("/", (req, res) => {
+    Book.find().then(books => res.send(books)).catch((error) => {
+        res.status(500).send("Je n'ai pas pu récupérer les livres");
+    })
+})
+
+//GET THE BOOK BY ID
+router.get("/:bookId", async (req,res) => {
+    const book = await Book.findById(req.params.bookId);
+    if(!book) res.status(404).send("Livre non trouvé");
+    res.send(book);
+});
+
+//UPDATE BOOK BASED ON ID
+router.put('/:bookId', async(req,res) => {
+    const updatedBook = await Book.findByIdAndUpdate(req.params.bookId, {
+        name:req.body.bookName,
+        author:{
+            name:req.body.authorName,
+            age:req.body.authorAge,
+        },
+        gender: req.body.gender
+    }, {new: true});
+    if(!updatedBook) res.status(404).send("Livre non trouvé");
+    res.send(updatedBook);
+});
+
+//DELETE A BOOK BASED ON ID
+router.delete('/:bookId', async(req,res) => {
+    const book = await Book.findByIdAndRemove(req.params.bookId);
+    if(!book) res.status(404).send("Livre non trouvé");
+    res.send(book);
+});
+
+
 module.exports = router;
