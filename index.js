730037e28b1aaa340ad6c54c06c55e6aa9435315
unknown <clementherpe@gmail.com>
2022-03-06 16:42:18 +0100

    Winston logger

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require('express');
+//Mongoose nous facilite les opérations sur mongoDB
 const mongoose = require('mongoose');
+//On utilise winston pour log les erreurs en console et en fichier
+const winston = require('winston');
 const app = express();
+
 require('dotenv').config();
 const booksRoute = require('./routes/books');
 
@@ -10,18 +14,37 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 
+//create a logger
+const logger = winston.createLogger({
+    level: 'info',
+    transports: [
+        new winston.transports.Console({
+            format:winston.format.combine(
+                winston.format.colorize({all:true})
+            )
+        }),
+        new winston.transports.File({ filename: 'error.log', level:'error'}),
+    ],
+    exceptionHandlers:[
+        new winston.transports.File({ filename: 'exeption.log'}),
+    ]
+})
+
 //routes
 app.use('/api/books', booksRoute);
+
+//Pour log des exeptions
+//throw new Error();
 
 //Connect to mongoDBAtlas
 mongoose.connect(process.env.MONGO_URL, {useNewUrlParser:true})
 .then(() => {
-    console.log("Connected to mongoDB");
+    logger.log("info", "Connected to mongoDB");
 }).catch((error) => { 
-    console.log("oops !", error);
+    logger.log("error", "La connexion à la BDD a échoué");
 });
 
 //Start server
 app.listen(PORT, () => {
-    console.log("Server started at port ", PORT);
+    logger.warn(`Server started at port ${PORT}`);
 });
